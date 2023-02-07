37359af9a89bcdbdf18087f8a27e92d47cb38452
Max Tan <105672769+MaxTan1228@users.noreply.github.com>
2023-02-07 10:46:20 -0700

    checkpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("test2");
+import express from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+	console.log("Listening on port 3000!");
+});
+
+app.get("/", (req, res) => {
+	res.send("hello world");
+});
