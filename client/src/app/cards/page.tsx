111c3496831d748e16da6b9e0cff328ab3b656b9
MaxTan <maxxontan@gmail.com>
2023-03-29 21:08:48 -0600

    fetch and log cards

--- client/src/app/cards/page.tsx
+++ client/src/app/cards/page.tsx
@@ -1,3 +1,9 @@
-export default function CardsPage() {
+import { createSupabaseServerClient } from "@/utils/supabase/supabaseServer";
+
+export default async function CardsPage() {
+  const supabase = createSupabaseServerClient();
+  const { data: cards } = await supabase.from("card").select();
+  console.log(cards);
+
   return <main>All Cards</main>;
 }
