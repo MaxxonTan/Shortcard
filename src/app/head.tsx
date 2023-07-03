a2cc6729893d07c85dd7238f946eee45df98c126
MaxxonTan <maxxontan@gmail.com>
2023-07-02 19:47:00 -0600

    add meta tags to root layout

--- src/app/head.tsx
+++
@@ -1,9 +0,0 @@
-export default function Head() {
-  return (
-    <>
-      <title>Shortcard</title>
-      <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <meta name="description" content="Generate birthday cards." />
-    </>
-  );
-}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Playfair_Display, Roboto_Flex } from "next/font/google";
 import SupabaseListener from "@/components/supabase/supabaseListener";
 import SupabaseProvider from "@/components/supabase/supabaseProvider";
 import { createSupabaseServerClient } from "@/utils/supabase/supabaseServer";
+import { Metadata } from "next";
 
 const playfairDisplay = Playfair_Display({
   subsets: ["latin"],
@@ -14,6 +15,26 @@ const roboto = Roboto_Flex({
   subsets: ["latin"],
   variable: "--font-roboto",
 });
+
+export const metadata: Metadata = {
+  title: "Shortcard",
+  description: "Generate birthday cards.",
+  openGraph: {
+    title: "Shortcard",
+    description: `Generate birthday cards instantly!`,
+    url: "https://shortcard.vercel.app/",
+    siteName: "shortcard.vercel.app",
+    images: [
+      {
+        url: "/icon.jpg",
+        width: 1000,
+        height: 1000,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default async function RootLayout({
   children,
